6e7b02995f1839a34a85053f745c36c9997011b3
Josh Tombs <joshtombs@gmail.com>
2015-12-03 15:43:28 -0500

    Change old DB file to new, and add new field for location

--- LMS_db.sql
+++ LMS_db.sql
@@ -1,82 +1,77 @@
-DROP DATABASE LMS;
+DROP DATABASE LMS_v2;
 
-CREATE DATABASE LMS;
+CREATE DATABASE LMS_v2;
 
-USE LMS;
+USE LMS_v2;
 
-/*CREATE TABLE t_Sensor_Metadata(
-	Sensor_ID		int,
-    Name 			varchar(30),
-    Manufacturer 	varchar(30),
-    Accuracy 		float(5,2),
-    Units			varchar(30),
-    Min_value 		float(4,2),
-    Max_value 		float(4,2),
-    Description		blob,
-    PRIMARY KEY (Sensor_ID));*/
+CREATE TABLE sensors(
+	sensor_id   	INT,
+	sensor_type		VARCHAR(15),
+	update_rate 	INT,
+	location        VARCHAR(20),
+	PRIMARY KEY (sensor_id)
+);
 
-CREATE TABLE t_Sensor_Data(
-	time_recorded 	datetime,
-    #Sensor_ID		int,
-	wind_speed 		float(4,2),
-	wind_direction 	int,
-	rainfall 		float(4,2), 
-	water_level 	float(4,2),
-	water_temp 		float(5,2), 
-	ambient_temp 	float(5,2), 
-	humidity 		int, 
-	flow_rate 		float(4,2), 
-		PRIMARY KEY (Time_Recorded));/*,
-		FOREIGN KEY (Sensor_ID)
-			REFERENCES t_Sensor_Metadata(Sensor_ID));*/
-		
-CREATE TABLE t_Contact_Info (
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    phone_number CHAR(11),
-    email VARCHAR(40),
-    #admin TINYINT(1),
-    PRIMARY KEY (First_Name , Last_Name)
+CREATE TABLE sensordata(
+	time_recorded 	DATETIME,
+	sensor_id       INT,
+	value 			FLOAT(5,2),
+	PRIMARY KEY (time_recorded, sensor_id),
+	FOREIGN KEY (sensor_id) REFERENCES sensors(sensor_id)
+);
+
+INSERT INTO sensors VALUES (1, 'wind speed', 15, 'first');
+INSERT INTO sensors VALUES (2, 'wind direction', 30, 'second');
+INSERT INTO sensors VALUES (3, 'rainfall', 60, 'third');
+INSERT INTO sensors VALUES (4, 'water level', 20, 'fourth');
+INSERT INTO sensors VALUES (5, 'water temp', 60, 'fifth');
+INSERT INTO sensors VALUES (6, 'ambient temp', 20, 'sixth');
+INSERT INTO sensors VALUES (7, 'humidity', 60, 'seventh');
+INSERT INTO sensors VALUES (8, 'flow rate', 30, 'eighth');
+INSERT INTO sensors VALUES (9, 'wind speed', 20, 'ninth');
+
+INSERT INTO sensordata VALUES ('2015-10-31 11:45:24', 1, 30.22);
+INSERT INTO sensordata VALUES ('2015-11-2 12:00:24', 1, 32.22);
+INSERT INTO sensordata VALUES ('2015-11-3 12:15:24', 1, 31.54);
+INSERT INTO sensordata VALUES ('2015-11-4 12:00:24', 2, 32.22);
+INSERT INTO sensordata VALUES ('2015-11-4 12:00:25', 1, 35);
+INSERT INTO sensordata VALUES ('2015-11-4 12:00:27', 1, 37);
+INSERT INTO sensordata VALUES ('2015-11-4 12:02:27', 9, 41);
+INSERT INTO sensordata VALUES ('2015-11-4 14:03:09', 3, 21.21);
+INSERT INTO sensordata VALUES ('2015-11-4 11:40:34', 4, 45.7);
+
+CREATE TABLE contact (
+    first_name 		VARCHAR(30),
+    last_name 		VARCHAR(30),
+    phone_number 	CHAR(11),
+    email 			VARCHAR(40),
+    PRIMARY KEY (first_name , last_name)
 );
     
-/*CREATE TABLE t_Contact_Conditions(
-	First_Name 			varchar(30), 
-	Last_Name 			varchar(30),
-	Max_Wind_Speed 		float(4,2), 
-	Max_Rainfall 		float(4,2), 
-	Max_Water_Level 	float(4,2), 
-	Max_Water_Temp 		float(5,2), 
-	Max_Ambient_Temp 	float(5,2), 
-	Max_Humidity 		int, 
-	Max_Flow_Rate 		float(4,2), 
-    FOREIGN KEY (First_Name,Last_Name)
-		REFERENCES t_Contact_Info(First_Name, Last_Name));*/
-    
-/*CREATE TABLE t_Admin_Info(
-	First_Name 	varchar(30),
-    Last_Name	varchar(30),
-	Username 	varchar(20),
-    Password 	varchar(20),
-    PRIMARY KEY (username),
-	FOREIGN KEY (First_Name,Last_Name)
-		REFERENCES t_Contact_Info(First_Name, Last_Name, Admin));*/
+CREATE TABLE admin(
+	username 		VARCHAR(30),
+    password_digest	VARCHAR(60),
+	PRIMARY KEY (username));
 
-INSERT INTO t_Sensor_Data Values
-	('2015-10-31 11:45:24', 30.22, 1, 5.2, 22, 69, 55, 10, 5);
+DELIMITER $$
+CREATE TRIGGER limit_admin 
+BEFORE INSERT
+ON admin
+FOR EACH ROW
+BEGIN
+	SELECT COUNT(*) INTO @cnt FROM Admin;
+	IF @cnt > 0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "You can not have more than one admin.";
+	END IF;
+END
+$$
+DELIMITER ;
 
-INSERT INTO t_Sensor_Data Values
-	('2015-11-2 1:23:45', 1.333, 8, 2.444, 22.567, 80.4321, 41.1324, 40, 10.314);
-INSERT INTO t_Sensor_Data Values
-	('2015-11-3 11:23:45', 1.444, 7, 1.444, 12.567, 81.4321, 43.1324, 20, 5.314);
-INSERT INTO t_Sensor_Data Values
-	('2015-11-4 2:23:45', 1.555, 9, 3.444, 14.567, 82.4321, 47.1324, 11, 6.314);
-
-INSERT INTO t_Contact_Info Values 
+INSERT INTO contact VALUES 
 	('Jeffrey', 'Magina', 1234567890, 'jmagina@umassd.edu');
 
-INSERT INTO t_Contact_Info Values 
+INSERT INTO contact VALUES 
 	('Joshua', 'Tombs', 987654321, 'jtombs@umassd.edu');
-    
-/*INSERT INTO t_Sensor_Data Values
-	( '2015-11-4 1:25:45', 120, 30, 5.2, 22, 69, 55, 10, 5);
 
+INSERT INTO admin VALUES
+	('admin', '$2a$10$bdqXZ8ZdyKO7NP7ERmgEC.ZSkJL16HesTat63QmRTPcPlIXYiOwcK');
